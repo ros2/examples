c464aa69e88dfc342e13990756ac3ab3cf197593
William Woodall <william@osrfoundation.org>
2014-08-13 14:44:36 -0700

    use callback in subscriber

--- userland/src/subscriber.cpp
+++ userland/src/subscriber.cpp
@@ -3,10 +3,15 @@
 #include "rclcpp/executor/SingleThreadExecutor.h"
 #include "simple_msgs/Int32.h"
 
+void callback(const simple_msgs::Int32 *msg)
+{
+    std::cout << "Got message: " << msg->data << std::endl;
+}
+
 int main(int argc, char** argv)
 {
   rclcpp::Node *node = rclcpp::create_node();
-  node->create_subscriber<simple_msgs::Int32>("topic_name");
+  node->create_subscriber<simple_msgs::Int32>("topic_name", callback);
 
   rclcpp::executor::SingleThreadExecutor *executor = new rclcpp::executor::SingleThreadExecutor();
 
