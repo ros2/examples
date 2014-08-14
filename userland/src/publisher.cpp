8dca4b7595fe4192cb3550d7de0e6b811d8ebed5
Dirk Thomas <dthomas@osrfoundation.org>
2014-08-14 16:12:19 -0700

    update publisher/subscriber to use AllPrimitiveTypes message

--- userland/src/publisher.cpp
+++ userland/src/publisher.cpp
@@ -5,20 +5,33 @@
 
 #include "rclcpp/Node.h"
 #include "rclcpp/Publisher.h"
-#include "simple_msgs/Int32.h"
+#include "simple_msgs/AllPrimitiveTypes.h"
 
 
 int main(int argc, char** argv)
 {
   rclcpp::Node* n = rclcpp::create_node();
-  rclcpp::Publisher<simple_msgs::Int32>* p = n->create_publisher<simple_msgs::Int32>("topic_name");
-  simple_msgs::Int32 ros_msg;
+  rclcpp::Publisher<simple_msgs::AllPrimitiveTypes>* p = n->create_publisher<simple_msgs::AllPrimitiveTypes>("topic_name");
+  simple_msgs::AllPrimitiveTypes ros_msg;
 
   int number_of_msgs = 1000000;
 
   auto start = std::chrono::steady_clock::now();
   for (int i = 1; i < number_of_msgs; ++i) {
-    ros_msg.data = i;
+    ros_msg.my_bool = i % 2;
+    ros_msg.my_byte = i % 256;
+    ros_msg.my_char = i % 256;
+    ros_msg.my_float32 = 0.1f + i;
+    ros_msg.my_float64 = 0.1 + i;
+    ros_msg.my_int8 = i;
+    ros_msg.my_uint8 = i;
+    ros_msg.my_int16 = i;
+    ros_msg.my_uint16 = i;
+    ros_msg.my_int32 = i;
+    ros_msg.my_uint32 = i;
+    ros_msg.my_int64 = i;
+    ros_msg.my_uint64 = i;
+    ros_msg.my_string = "foo " + std::to_string(i);
     p->publish(ros_msg);
     // if (i % 100000 == 0) {
       std::cout << "published ros msg #" << i << std::endl;
--- userland/src/subscriber.cpp
+++ userland/src/subscriber.cpp
@@ -1,17 +1,31 @@
 #include "rclcpp/Node.h"
 #include "rclcpp/Publisher.h"
 #include "rclcpp/executor/SingleThreadExecutor.h"
-#include "simple_msgs/Int32.h"
+#include "simple_msgs/AllPrimitiveTypes.h"
 
-void callback(const simple_msgs::Int32 *msg)
+void callback(const simple_msgs::AllPrimitiveTypes *msg)
 {
-    std::cout << "Got message: " << msg->data << std::endl;
+    std::cout << "Got message:" << std::endl;
+    std::cout << "  my_bool: " << msg->my_bool << std::endl;
+    std::cout << "  my_byte: " << msg->my_byte << std::endl;
+    std::cout << "  my_char: " << msg->my_char << std::endl;
+    std::cout << "  my_float32: " << msg->my_float32 << std::endl;
+    std::cout << "  my_float64: " << msg->my_float64 << std::endl;
+    std::cout << "  my_int8: " << msg->my_int8 << std::endl;
+    std::cout << "  my_uint8: " << msg->my_uint8 << std::endl;
+    std::cout << "  my_int16: " << msg->my_int16 << std::endl;
+    std::cout << "  my_uint16: " << msg->my_uint16 << std::endl;
+    std::cout << "  my_int32: " << msg->my_int32 << std::endl;
+    std::cout << "  my_uint32: " << msg->my_uint32 << std::endl;
+    std::cout << "  my_int64: " << msg->my_int64 << std::endl;
+    std::cout << "  my_uint64: " << msg->my_uint64 << std::endl;
+    std::cout << "  my_string: " << msg->my_string << std::endl;
 }
 
 int main(int argc, char** argv)
 {
   rclcpp::Node *node = rclcpp::create_node();
-  node->create_subscriber<simple_msgs::Int32>("topic_name", callback);
+  node->create_subscriber<simple_msgs::AllPrimitiveTypes>("topic_name", callback);
 
   rclcpp::executor::SingleThreadExecutor *executor = new rclcpp::executor::SingleThreadExecutor();
 
