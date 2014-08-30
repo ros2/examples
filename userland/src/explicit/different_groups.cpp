4393d7565b80c8b224a093664ac5708672c4ed7c
William Woodall <william@osrfoundation.org>
2014-08-29 17:54:02 -0700

    modify examples and disable some

--- userland/src/explicit/different_groups.cpp
+++ userland/src/explicit/different_groups.cpp
@@ -1,17 +1,35 @@
+#include <functional>
 #include <iostream>
+#include <thread>
 
 #include <rclcpp/rclcpp.hpp>
 
 using rclcpp::callback_group::CallbackGroupType;
+using std::chrono::steady_clock;
+typedef std::chrono::duration<float> floating_seconds;
 
 void on_timer_group1()
 {
-  std::cout << "In on_timer_group1." << std::endl;
+  auto this_id = rclcpp::thread_id;
+  auto start = steady_clock::now();
+  std::cout << "[1:" << this_id << "] Start" << std::endl;
+  rclcpp::sleep_for(0.001_s);
+  std::cout << "[1:" << this_id << "] Stop after "
+            << std::chrono::duration_cast<floating_seconds>(
+              steady_clock::now() - start).count()
+            << std::endl;
 }
 
 void on_timer_group2()
 {
-  std::cout << "In on_timer_group2." << std::endl;
+  auto this_id = rclcpp::thread_id;
+  auto start = steady_clock::now();
+  std::cout << "[2:" << this_id << "] Start" << std::endl;
+  rclcpp::sleep_for(0.001_s);
+  std::cout << "[2:" << this_id << "] Stop after "
+            << std::chrono::duration_cast<floating_seconds>(
+              steady_clock::now() - start).count()
+            << std::endl;
 }
 
 int main(int argc, char *argv[])
@@ -20,16 +38,21 @@ int main(int argc, char *argv[])
 
   auto node = rclcpp::Node::make_shared("my_node");
 
-  auto g1 = node->create_callback_group(CallbackGroupType::MutuallyExclusive);
-  auto g2 = node->create_callback_group(CallbackGroupType::Reentrant);
+  // auto g1 = node->create_callback_group(CallbackGroupType::MutuallyExclusive);
+  auto g2 = node->create_callback_group(CallbackGroupType::MutuallyExclusive);
+  // auto g2 = node->create_callback_group(CallbackGroupType::Reentrant);
 
-  auto timer1 = node->create_wall_timer(0.5_s, on_timer_group1, g1);
-  auto timer2 = node->create_wall_timer(0.5_s, on_timer_group2, g1);
+  // auto timer1 = node->create_wall_timer(2.0_s, on_timer_group1, g1);
+  // auto timer2 = node->create_wall_timer(0.25_s, on_timer_group2, g2);
+  auto timer3 = node->create_wall_timer(0.001_s, on_timer_group1, g2);
 
-  rclcpp::executors::MultiThreadedExecutor executor;
+  // rclcpp::executors::MultiThreadedExecutor executor;
+  rclcpp::executors::SingleThreadedExecutor executor;
   executor.add_node(node);
 
   executor.spin();
+
+  rclcpp::utilities::sleep_for(std::chrono::seconds(1));
 
   return 0;
 }
--- userland/src/explicit/two_nodes.cpp
+++ userland/src/explicit/two_nodes.cpp
@@ -12,9 +12,10 @@ void on_message(const simple_msgs::String::ConstPtr &msg)
 
 void on_timer(rclcpp::Publisher::SharedPtr &publisher, int &i)
 {
-  simple_msgs::String::Ptr msg;
+  simple_msgs::String::Ptr msg(new simple_msgs::String());
   msg->data = "Hello World: " + std::to_string(i++);
   if (publisher) {
+    std::cout << "Publishing: '" << msg->data << "'" << std::endl;
     publisher->publish(msg);
   } else {
     std::cerr << "Invalid publisher!" << std::endl;
@@ -30,19 +31,20 @@ int main(int argc, char *argv[])
 
   auto publisher = node1->create_publisher<simple_msgs::String>("chatter", 7);
   auto subscription = \
-    node2->create_publisher<simple_msgs::String>("chatter", 7, on_message);
+    node2->create_subscription<simple_msgs::String>("chatter", 7, on_message);
 
   int i = 0;
   using std::placeholders::_1;
-  auto timer_bound = std::bind(on_timer, _1, std::ref(publisher), std::ref(i));
-  auto timer = node1->create_timer(0.5, timer_bound);
+  auto timer_bound = std::bind(on_timer, std::ref(publisher), std::ref(i));
+  auto timer = node1->create_wall_timer(0.5_s, timer_bound);
 
-  auto executor = rclcpp::executors::MultiThreadedExecutor();
+  // rclcpp::executors::MultiThreadedExecutor executor;
+  rclcpp::executors::SingleThreadedExecutor executor;
 
-  executor.register_node(node1);
-  executor.register_node(node2);
+  executor.add_node(node1);
+  executor.add_node(node2);
 
-  executor.exec();
+  executor.spin();
 
   return 0;
 }
