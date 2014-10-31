a4d4af9759ae42881219f58abb2efd1b114bf3b6
William Woodall <william@osrfoundation.org>
2014-10-30 17:01:19 -0700

    small changes to the different_groups example

--- userland/src/explicit/different_groups.cpp
+++ userland/src/explicit/different_groups.cpp
@@ -14,9 +14,9 @@ void on_timer_group1()
   auto start = steady_clock::now();
   std::cout << "[1:" << this_id << "] Start" << std::endl;
   rclcpp::sleep_for(0.001_s);
+  auto diff = steady_clock::now() - start;
   std::cout << "[1:" << this_id << "] Stop after "
-            << std::chrono::duration_cast<floating_seconds>(
-              steady_clock::now() - start).count()
+            << std::chrono::duration_cast<floating_seconds>(diff).count()
             << std::endl;
 }
 
@@ -26,9 +26,9 @@ void on_timer_group2()
   auto start = steady_clock::now();
   std::cout << "[2:" << this_id << "] Start" << std::endl;
   rclcpp::sleep_for(0.001_s);
+  auto diff = steady_clock::now() - start;
   std::cout << "[2:" << this_id << "] Stop after "
-            << std::chrono::duration_cast<floating_seconds>(
-              steady_clock::now() - start).count()
+            << std::chrono::duration_cast<floating_seconds>(diff).count()
             << std::endl;
 }
 
@@ -43,8 +43,8 @@ int main(int argc, char *argv[])
   // auto g2 = node->create_callback_group(CallbackGroupType::Reentrant);
 
   // auto timer1 = node->create_wall_timer(2.0_s, on_timer_group1, g1);
-  // auto timer2 = node->create_wall_timer(0.25_s, on_timer_group2, g2);
-  auto timer3 = node->create_wall_timer(0.001_s, on_timer_group1, g2);
+  auto timer2 = node->create_wall_timer(0.25_s, on_timer_group2, g2);
+  // auto timer3 = node->create_wall_timer(0.001_s, on_timer_group1, g2);
 
   // rclcpp::executors::MultiThreadedExecutor executor;
   rclcpp::executors::SingleThreadedExecutor executor;
