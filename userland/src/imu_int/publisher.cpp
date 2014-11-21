eb02012e9c05aa9bc4b8b18893748c968ca784d2
Victor Mayoral Vilches <v.mayoralv@gmail.com>
2014-11-21 12:14:42 -0800

    Add a pub/sub Vector3Int example
    This code interoperates with Tinq's DDS
    implementation on showed that there's an
    issue with 64 bit types.

---
+++ userland/src/imu_int/publisher.cpp
@@ -0,0 +1,64 @@
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <sys/time.h>
+#include <thread>
+
+#include <rclcpp/rclcpp.hpp>
+#include <simple_msgs/Vector3Int.h>
+#include "userland/command_line_arguments.h"
+
+
+template<typename T>
+int publish(rclcpp::Node::SharedPtr node, void (*set_data_func)(typename T::Ptr&, size_t))
+{
+  auto p = node->create_publisher<T>("imu", 1000);
+  typename T::Ptr ros_msg(new T());
+
+  auto start = std::chrono::steady_clock::now();
+  rclcpp::WallRate rate(10);
+  size_t i = 1;
+  while (rclcpp::ok()) {
+    set_data_func(ros_msg, i);
+    p->publish(ros_msg);
+      std::cout << "published Vector3Int ROS msg #" << i << std::endl;
+    ++i;
+    rate.sleep();
+  }
+  auto end = std::chrono::steady_clock::now();
+
+  std::chrono::duration<float> diff = (end - start);
+  std::cout << "Runtime: " << diff.count() << " seconds" << std::endl;
+
+  return 0;
+}
+
+void set_accel_data(simple_msgs::Vector3Int::Ptr &ros_msg, size_t i)
+{
+  ros_msg->x = 1;
+  ros_msg->y = 2;
+  ros_msg->z = 3;  
+}
+
+template<typename T>
+void set_empty(typename T::Ptr &ros_msg, size_t i)
+{
+  ros_msg.reset(new T());
+}
+
+
+int main(int argc, char** argv)
+{
+  rclcpp::init(argc, argv);
+
+  if (has_argument(argv, argv + argc, "--help")) {
+    std::cout << "usage: " << argv[0] << std::endl;
+    print_message_usage();
+    return 0;
+  }
+
+  auto node = rclcpp::Node::make_shared("publisher");
+  return publish<simple_msgs::Vector3Int>(node, &set_accel_data);
+}
