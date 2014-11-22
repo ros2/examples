ebe99c380882706a17e3dddd26fc9f324a4c010e
Victor Mayoral Vilches <v.mayoralv@gmail.com>
2014-11-21 16:56:55 -0800

    Add Imu32 example
    This code sample sends over the wire Imu32
    messages (a translation of the ROS Imu msg
    into a 4-byte components message).
    
    This commit also adds a Time.msg to the
    simple_msgs/msg folder. As soon as #2 gets
    addressed this message definition could be
    removed.

---
+++ userland/src/imu/imu32/publisher.cpp
@@ -0,0 +1,96 @@
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <sys/time.h>
+#include <thread>
+
+#include <rclcpp/rclcpp.hpp>
+#include <simple_msgs/Imu32.h>
+#include "userland/command_line_arguments.h"
+
+
+template<typename T>
+int publish(rclcpp::Node::SharedPtr node, void (*set_data_func)(typename T::Ptr&, size_t))
+{
+  auto p = node->create_publisher<T>("imu", 1000);
+  typename T::Ptr ros_msg(new T());
+
+  auto start = std::chrono::steady_clock::now();
+  rclcpp::WallRate rate(10);
+  size_t i = 1;
+  while (rclcpp::ok()) {
+    set_data_func(ros_msg, i);
+    p->publish(ros_msg);
+      std::cout << "published Imu32 ROS msg #" << i << std::endl;
+    ++i;
+    rate.sleep();
+  }
+  auto end = std::chrono::steady_clock::now();
+
+  std::chrono::duration<float> diff = (end - start);
+  std::cout << "Runtime: " << diff.count() << " seconds" << std::endl;
+
+  return 0;
+}
+
+void set_imu_data(simple_msgs::Imu32::Ptr &ros_msg, size_t i)
+{
+  // Define the header
+  ros_msg->header.seq = i;
+  ros_msg->header.stamp.sec = 0;
+  ros_msg->header.stamp.nanosec = 0;
+  ros_msg->header.frame_id = "";
+
+  // Define the orientation
+  ros_msg->orientation.x =0;
+  ros_msg->orientation.y =0;
+  ros_msg->orientation.z =0;
+  ros_msg->orientation.w =0;
+  
+
+  // Define the orientation_covariance
+  for (int i = 0; i<9; i++){
+    ros_msg->orientation_covariance[i] = 0;
+  }
+  // Define the angular_velocity
+  ros_msg->angular_velocity.x = 0;
+  ros_msg->angular_velocity.y = 0;
+  ros_msg->angular_velocity.z = 0;  
+
+  // Define the angular_velocity_covariance
+  for (int i = 0; i<9; i++){
+    ros_msg->angular_velocity_covariance[i] = 0;
+  }  
+  // Define the linear_acceleration
+  ros_msg->linear_acceleration.x = 1 + i;
+  ros_msg->linear_acceleration.y = 2 + i;
+  ros_msg->linear_acceleration.z = 3 + i;  
+  
+  // Define the linear_acceleration_covariance
+  for (int i = 0; i<9; i++){
+    ros_msg->linear_acceleration_covariance[i] = 0;
+  }
+}
+
+template<typename T>
+void set_empty(typename T::Ptr &ros_msg, size_t i)
+{
+  ros_msg.reset(new T());
+}
+
+
+int main(int argc, char** argv)
+{
+  rclcpp::init(argc, argv);
+
+  if (has_argument(argv, argv + argc, "--help")) {
+    std::cout << "usage: " << argv[0] << std::endl;
+    print_message_usage();
+    return 0;
+  }
+
+  auto node = rclcpp::Node::make_shared("publisher");
+  return publish<simple_msgs::Imu32>(node, &set_imu_data);
+}
