9fb3c19f24719095ba1ef982dac380f841815714
Esteve Fernandez <esteve@osrfoundation.org>
2015-04-20 10:30:06 -0700

    Use spin_until_future_complete helper functions instead of wait_for_future

--- userland/src/parameters.cpp
+++ userland/src/parameters.cpp
@@ -7,23 +7,6 @@
 
 #include <rclcpp/rclcpp.hpp>
 
-template<typename ParameterTypeT>
-ParameterTypeT
-wait_for_future(rclcpp::node::Node::SharedPtr & node, std::shared_future<ParameterTypeT> & future)
-{
-  std::future_status status;
-  do {
-    rclcpp::spin_some(node);
-    status = future.wait_for(std::chrono::milliseconds(100));
-  } while (status != std::future_status::ready && rclcpp::ok());
-
-  if (std::future_status::ready == status) {
-    return future.get();
-  }
-
-  throw std::runtime_error("Invalid future status");
-}
-
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
@@ -33,16 +16,16 @@ int main(int argc, char ** argv)
   auto node = rclcpp::Node::make_shared(node_name);
 
   auto f1 = node->async_has_parameter(node_name, "foo");
-  bool found = wait_for_future<bool>(node, f1);
+  bool found = rclcpp::spin_until_future_complete<bool>(node, f1).get();
   std::cout << "Parameter (foo) found: " << (found ? "true" : "false") << std::endl;
 
   int64_t value1 = 1234;
   auto f2 = node->async_set_parameter(node_name, "foo", value1);
-  wait_for_future<bool>(node, f2);
+  rclcpp::spin_until_future_complete<bool>(node, f2);
   std::cout << "Setting value: " << value1 << " for parameter (foo)" << std::endl;
 
   auto f3 = node->async_has_parameter(node_name, "foo");
-  found = wait_for_future<bool>(node, f3);
+  found = rclcpp::spin_until_future_complete<bool>(node, f3).get();
   std::cout << "Parameter (foo) found: " << (found ? "true" : "false") << std::endl;
 
   const std::vector<rclcpp::parameter::ParameterContainer> kv = {
@@ -50,15 +33,15 @@ int main(int argc, char ** argv)
   };
 
   auto f4 = node->async_set_parameters(node_name, kv);
-  wait_for_future<bool>(node, f4);
+  rclcpp::spin_until_future_complete<bool>(node, f4);
 
   auto f5 = node->async_get_parameter<int64_t>(node_name, "foo");
-  int64_t value2 = wait_for_future<int64_t>(node, f5);
+  int64_t value2 = rclcpp::spin_until_future_complete<int64_t>(node, f5).get();
   std::cout << "Value for parameter (foo): " << value2 << std::endl;
 
   const std::vector<std::string> pn = {"foo"};
   auto f6 = node->async_get_parameters(node_name, pn);
-  auto multi_value = wait_for_future<std::vector<rclcpp::parameter::ParameterContainer>>(node, f6);
+  auto multi_value = rclcpp::spin_until_future_complete<std::vector<rclcpp::parameter::ParameterContainer>>(node, f6).get();
 
   for (auto v : multi_value) {
     std::cout << "Value for parameter (foo) (multi value): " << v.get_value<int64_t>() << std::endl;
@@ -69,7 +52,7 @@ int main(int argc, char ** argv)
     std::cout << "Value for parameter (foo) (callback): " << f.get() << std::endl;
   }
     );
-  wait_for_future<int64_t>(node, f7);
+  rclcpp::spin_until_future_complete<int64_t>(node, f7);
   rclcpp::spin(node);
 
   return 0;
