0733849ffc03c819961de0a7427cded00dacc6d5
Tully Foote <tfoote@osrfoundation.org>
2015-12-11 10:12:39 -0800

    subscribing to bound method in example

---
+++ rclcpp_examples/src/topics/listener_class.cpp
@@ -0,0 +1,45 @@
+// Copyright 2014 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <iostream>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+
+#include "std_msgs/msg/string.hpp"
+
+class Chatter
+{
+public:
+  void chatterCallback(const std_msgs::msg::String::SharedPtr msg)
+  {
+    std::cout << "I heard: [" << msg->data << "]" << std::endl;
+  }
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("listener");
+
+  Chatter chatter;  // object to call method on
+
+  auto sub = node->create_subscription<std_msgs::msg::String>(
+    "chatter", std::bind(&Chatter::chatterCallback, &chatter,
+    std::placeholders::_1), rmw_qos_profile_default);
+
+  rclcpp::spin(node);
+
+  return 0;
+}
