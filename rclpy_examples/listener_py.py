1df40225eb0c0e6e8d1a201093fe90b0a247c898
Mikael Arguedas <mikael@osrfoundation.org>
2016-06-22 11:05:03 -0700

    import message at module level (#112)

--- rclpy_examples/listener_py.py
+++ rclpy_examples/listener_py.py
@@ -15,8 +15,9 @@
 import sys
 
 import rclpy
-
 from rclpy.qos import qos_profile_default
+
+from std_msgs.msg import String
 
 
 def chatter_callback(msg):
@@ -28,10 +29,6 @@ def main(args=None):
         args = sys.argv
 
     rclpy.init(args)
-
-    # TODO(wjwwood): move this import back to the top of the file when
-    # it is possible to import the messages before rclpy.init().
-    from std_msgs.msg import String
 
     node = rclpy.create_node('listener')
 
