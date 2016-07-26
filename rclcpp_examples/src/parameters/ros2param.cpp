1fbbc0dfaf16565fcc0d99347bdfad55d06fe02b
Rohan Agrawal <rohan@osrfoundation.org>
2016-07-26 09:13:51 -0700

    Added list verb functionality

--- rclcpp_examples/src/parameters/ros2param.cpp
+++ rclcpp_examples/src/parameters/ros2param.cpp
@@ -18,13 +18,14 @@
 
 #include "rclcpp/rclcpp.hpp"
 
-#define USAGE "USAGE:\n  ros2param get <node/variable>\n  ros2param set <node/variable> <value>"
+#define USAGE "USAGE:\n  ros2param get <node/variable>\n  ros2param set <node/variable> <value>\n  ros2param list <node>"
 
 typedef enum
 {
   PARAM_NONE,
   PARAM_GET,
   PARAM_SET,
+  PARAM_LIST,
 } param_operation_t;
 
 rclcpp::parameter::ParameterVariant
@@ -36,15 +37,24 @@ parse_args(int argc, char ** argv, std::string & remote_node, param_operation_t 
 
   std::string verb = argv[1];
   std::string name = argv[2];
-  size_t slash = name.find('/');
-  if ((slash == std::string::npos) ||
-    (slash == 0) ||
-    (slash == (name.size() - 1)))
-  {
+  std::string variable;
+
+  if (verb == "list") {
+    op = PARAM_LIST;
+    remote_node = name;
     return rclcpp::parameter::ParameterVariant();
+  } else {
+    size_t slash = name.find('/');
+    if ((slash == std::string::npos) ||
+        (slash == 0) ||
+        (slash == (name.size() - 1)))
+    {
+      return rclcpp::parameter::ParameterVariant();
+    }
+    remote_node = name.substr(0, slash);
+    variable = name.substr(slash + 1, name.size() - slash - 1);
   }
-  remote_node = name.substr(0, slash);
-  std::string variable = name.substr(slash + 1, name.size() - slash - 1);
+
 
   if ((verb == "get") && (argc == 3)) {
     op = PARAM_GET;
@@ -91,6 +101,7 @@ int main(int argc, char ** argv)
   auto node = rclcpp::Node::make_shared("ros2param");
   auto parameters_client =
     std::make_shared<rclcpp::parameter_client::AsyncParametersClient>(node, remote_node);
+  auto parameter_service = std::make_shared<rclcpp::parameter_service::ParameterService>(node);
 
   if (op == PARAM_GET) {
     auto get_parameters_result = parameters_client->get_parameters({var.get_name()});
@@ -124,6 +135,21 @@ int main(int argc, char ** argv)
     if (set_result != rclcpp::executor::FutureReturnCode::SUCCESS) {
       fprintf(stderr, "Failed to set parameter\n");
       return 1;
+    }
+  } else if (op == PARAM_LIST) {
+    auto list_parameters_result = parameters_client->list_parameters({},10);
+    auto list_result = rclcpp::spin_until_future_complete(
+        node, list_parameters_result, std::chrono::milliseconds(10000));
+    if (list_result == rclcpp::executor::FutureReturnCode::SUCCESS) {
+      printf("Node %s has %d parameters:\n", remote_node.c_str(), (int) list_parameters_result.get().names.size());
+      for (auto name : list_parameters_result.get().names) {
+        printf("%s\n", name.c_str());
+      }
+    } else if (list_result == rclcpp::executor::FutureReturnCode::TIMEOUT){
+      printf("Timed out trying to list parameters from node\n");
+      return 1;
+    } else {
+      printf("Error listing parameters\n");
     }
   } else {
     fprintf(stderr, "%s\n", USAGE);
