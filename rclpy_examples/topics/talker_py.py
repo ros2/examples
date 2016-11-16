dbedde51ee57d082bfe1888149d4652c63d78030
Mikael Arguedas <mikael.arguedas@gmail.com>
2016-11-15 16:16:16 -0800

    restore original talker_py.py

--- rclpy_examples/topics/talker_py.py
+++ rclpy_examples/topics/talker_py.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import sys
+from time import sleep
 
 import rclpy
 
@@ -21,19 +22,7 @@ from rclpy.qos import qos_profile_default
 from std_msgs.msg import String
 
 
-i = 1
-data = ''
-
-
-def timer_cb():
-    global i, data
-
-    data = 'Hello World: {0}'.format(i)
-    i += 1
-
-
 def main(args=None):
-    global i, data
     if args is None:
         args = sys.argv
 
@@ -42,25 +31,17 @@ def main(args=None):
     node = rclpy.create_node('talker')
 
     chatter_pub = node.create_publisher(String, 'chatter', qos_profile_default)
-    timer = node.create_timer(1, timer_cb)
-    timer  # unused
+
     msg = String()
 
+    i = 1
     while True:
-        if i < 17:
-            msg.data = data
-            print('Publishing: "{0}"'.format(msg.data))
-            chatter_pub.publish(msg)
-        elif i == 17:
-            print('destroying publisher')
-            node.destroy_publisher(chatter_pub)
-            node.destroy_timer(timer)
-        if i == 3:
-            print('changing time period')
-            print('period before: %d' % timer.timer_period_ns)
-            timer.timer_period_ns = 0.33 * 1000 * 1000 * 1000
-            print('period after: %d' % timer.timer_period_ns)
-        rclpy.spin_once(node, 2)
+        msg.data = 'Hello World: {0}'.format(i)
+        i += 1
+        print('Publishing: "{0}"'.format(msg.data))
+        chatter_pub.publish(msg)
+        # TODO(wjwwood): need to spin_some or spin_once with timeout
+        sleep(1)
 
 if __name__ == '__main__':
     main()
