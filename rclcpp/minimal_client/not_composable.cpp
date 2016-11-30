3847ecdecb57aff99610b9d8d62b42ee1328f546
Morgan Quigley <morgan@osrfoundation.org>
2016-11-29 17:52:20 -0800

    style fixes and cleanup

--- rclcpp/minimal_client/not_composable.cpp
+++ rclcpp/minimal_client/not_composable.cpp
@@ -1,10 +1,25 @@
+// Copyright 2016 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
 #include "rclcpp/rclcpp.hpp"
 #include "examples_rclcpp_minimal_service/srv/add_two_ints.hpp"
 
 using Service = examples_rclcpp_minimal_service::srv::AddTwoInts;
 using Request = Service::Request;
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   auto node = rclcpp::node::Node::make_shared("minimal_subscriber");
@@ -18,15 +33,16 @@ int main(int argc, char *argv[])
   }
   auto request = std::make_shared<Request>();
   request->a = 41;
-  request->b =  1;
+  request->b = 1;
   auto result_future = client->async_send_request(request);
   if (rclcpp::spin_until_future_complete(node, result_future) !=
-      rclcpp::executor::FutureReturnCode::SUCCESS) {
+    rclcpp::executor::FutureReturnCode::SUCCESS)
+  {
     printf("service call failed :(\n");
     return 1;
   }
   auto result = result_future.get();
   printf("result of sending service request for %ld + %ld = %ld\n",
-         request->a, request->b, result->sum);
+    request->a, request->b, result->sum);
   return 0;
 }
--- rclcpp/minimal_service/not_composable.cpp
+++ rclcpp/minimal_service/not_composable.cpp
@@ -1,28 +1,38 @@
+// Copyright 2016 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
 #include "rclcpp/rclcpp.hpp"
 #include "examples_rclcpp_minimal_service/srv/add_two_ints.hpp"
 
-using Request  = examples_rclcpp_minimal_service::srv::AddTwoInts::Request;
-using Response = examples_rclcpp_minimal_service::srv::AddTwoInts::Response;
+using Service = examples_rclcpp_minimal_service::srv::AddTwoInts;
 
 void handle_service(
-    const std::shared_ptr<rmw_request_id_t> request_header,
-    const std::shared_ptr<Request> request,
-    const std::shared_ptr<Response> response)
+  const std::shared_ptr<rmw_request_id_t> request_header,
+  const std::shared_ptr<Service::Request> request,
+  const std::shared_ptr<Service::Response> response)
 {
   (void)request_header;
   printf("request: %ld + %ld\n", request->a, request->b);
   response->sum = request->a + request->b;
 }
 
-int main(int argc, char **argv)
+int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("minimal_service");
-  auto server =
-      node->create_service<examples_rclcpp_minimal_service::srv::AddTwoInts>(
-          "service", handle_service);
-
+  auto server = node->create_service<Service>("service", handle_service);
   rclcpp::spin(node);
-  
   return 0;
 }
