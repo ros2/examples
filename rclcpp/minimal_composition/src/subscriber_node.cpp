c1db1513269bf6715fe3f09082a57863dbec007c
Mikael Arguedas <mikael@osrfoundation.org>
2018-02-09 13:52:25 -0800

    use new class_loader header names

--- rclcpp/minimal_composition/src/subscriber_node.cpp
+++ rclcpp/minimal_composition/src/subscriber_node.cpp
@@ -26,6 +26,6 @@ SubscriberNode::SubscriberNode()
   });
 }
 
-#include "class_loader/class_loader_register_macro.h"
+#include "class_loader/register_macro.hpp"
 
 CLASS_LOADER_REGISTER_CLASS(SubscriberNode, rclcpp::Node)
