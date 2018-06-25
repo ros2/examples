2b1adc845831837c90b7a8b7c308b0a1dee4d6cf
Chris Lalancette <clalancette@openrobotics.org>
2018-06-25 14:26:06 +0000

    Manually destroy the subscription first.
    Signed-off-by: Chris Lalancette <clalancette@openrobotics.org>

--- rclcpp/minimal_subscriber/not_composable.cpp
+++ rclcpp/minimal_subscriber/not_composable.cpp
@@ -15,6 +15,8 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
+rclcpp::Node::SharedPtr g_node = nullptr;
+
 /* We do not recommend this style anymore, because composition of multiple
  * nodes in the same executable is not possible. Please see one of the subclass
  * examples for the "new" recommended styles. This example is only included
@@ -22,19 +24,22 @@
 
 void topic_callback(const std_msgs::msg::String::SharedPtr msg)
 {
-  RCLCPP_INFO(rclcpp::get_logger("minimal_subscriber"), "I heard: '%s'", msg->data.c_str())
+  RCLCPP_INFO(g_node->get_logger(), "I heard: '%s'", msg->data.c_str())
 }
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  // TODO(clalancette): We can't currently make g_node a global because of
-  // https://github.com/eProsima/Fast-RTPS/issues/235 .  Once that is fixed
-  // we may want to add another example that uses global nodes.
-  auto node = rclcpp::Node::make_shared("minimal_subscriber");
-  auto subscription = node->create_subscription<std_msgs::msg::String>
+  g_node = rclcpp::Node::make_shared("minimal_subscriber");
+  auto subscription = g_node->create_subscription<std_msgs::msg::String>
       ("topic", topic_callback);
-  rclcpp::spin(node);
+  rclcpp::spin(g_node);
   rclcpp::shutdown();
+  // TODO(clalancette): It would be better to remove both of these nullptr
+  // assignments and let the destructors handle it, but we can't because of
+  // https://github.com/eProsima/Fast-RTPS/issues/235 .  Once that is fixed
+  // we should probably look at removing these two assignments.
+  subscription = nullptr;
+  g_node = nullptr;
   return 0;
 }
