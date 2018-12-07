245ae8d6fd269d946eb8b7251af1509d6006fd67
Shane Loretz <shane.loretz@gmail.com>
2018-12-06 19:38:04 -0800

    Rclcpp action examples (#220)
    * Add minimal_action_server package
    
    Contains a non-composable implementation with global variables.
    
    * Add minimal_action_client package
    
    Contains a non-composable implementation.
    
    * Add action client example with feedback
    
    * async python action client example
    
    * goal -> future
    
    * fibb -> fib"
    
    * Syncronous action client example
    
    * No break statement
    
    * Update client examples to use separate rcl_action package
    
    * Add ClientGoalHandle to action client examplesj
    
    * Add action client with cancel example
    
    * python non-composable server example
    
    * [wip] Update action server cpp example
    
    * remove unnecessary event
    
    * create_action_server -> ActionServer
    
    * missing paren
    
    * Add example of multiple goals in parallel
    
    * No need for lock
    
    * Reentrant callback group for execute
    
    * create_action_client -> ActionClient
    
    * Fix copyright date
    
    * )
    
    * -)
    
    * Refactor action server cpp example
    
    * Fix action server cpp example
    
    Seed the fibonacci sequence and remove const.
    
    * Fix action server cpp example
    
    Forgot to increment in Fibonacci sequence loop.
    
    * Syntax fixes
    
    * node -> self
    
    * handle cb returns accept or reject
    
    * Update action client cpp example
    
    Return goal handle (containing future) when sending a goal.
    
    * Preempt goals
    
    * whitespace removal
    
    * execute returns result
    
    * Add missing resources
    
    * Syntax error
    
    * Add rcl_action dependency
    
    * Update maintainer
    
    * Use goal message getter and alias ResultResponse type
    
    * Make minimal_action_server work with rclcpp_action
    
    * Client and server communicate
    
    * handle_execute -> handle_accepted
    
    * Check if goal was rejected by server
    
    * Update example to check result
    
    * action client cancel example C++ works
    
    * misc changes to compile
    
    * misc client api changes
    
    * Remove python examples
    
    * Wait for action server

---
+++ rclcpp/minimal_action_server/README.md
@@ -0,0 +1,3 @@
+# Minimal action server cookbook recipes
+
+This package contains a few examples which show how to create action servers.
---
+++ rclcpp/minimal_action_server/not_composable.cpp
@@ -0,0 +1,111 @@
+// Copyright 2018 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <inttypes.h>
+#include <memory>
+#include "example_interfaces/action/fibonacci.hpp"
+#include "rclcpp/rclcpp.hpp"
+// TODO(jacobperron): Remove this once it is included as part of 'rclcpp.hpp'
+#include "rclcpp_action/rclcpp_action.hpp"
+
+using Fibonacci = example_interfaces::action::Fibonacci;
+using GoalHandleFibonacci = rclcpp_action::ServerGoalHandle<Fibonacci>;
+
+rclcpp_action::GoalResponse handle_goal(
+  const std::array<uint8_t, 16> & uuid, std::shared_ptr<const Fibonacci::Goal> goal)
+{
+  RCLCPP_INFO(rclcpp::get_logger("server"), "Got goal request with order %d", goal->order);
+  (void)uuid;
+  // Let's reject sequences that are over 9000
+  if (goal->order > 9000)
+  {
+    return rclcpp_action::GoalResponse::REJECT;
+  }
+  return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+}
+
+rclcpp_action::CancelResponse handle_cancel(
+  const std::shared_ptr<GoalHandleFibonacci> goal_handle)
+{
+  RCLCPP_INFO(rclcpp::get_logger("server"), "Got request to cancel goal");
+  (void)goal_handle;
+  return rclcpp_action::CancelResponse::ACCEPT;
+}
+
+void execute(
+  const std::shared_ptr<GoalHandleFibonacci> goal_handle)
+{
+  RCLCPP_INFO(rclcpp::get_logger("server"), "Executing goal");
+  rclcpp::Rate loop_rate(1);
+  const auto goal = goal_handle->get_goal();
+  auto feedback = std::make_shared<Fibonacci::Feedback>();
+  auto& sequence = feedback->sequence;
+  sequence.push_back(0);
+  sequence.push_back(1);
+  auto result_response = std::make_shared<Fibonacci::Result>();
+
+  for (int i = 1; (i < goal->order) && rclcpp::ok(); ++i)
+  {
+    // Check if there is a cancel request
+    if (goal_handle->is_canceling())
+    {
+      result_response->sequence = sequence;
+      goal_handle->set_canceled(result_response);
+      RCLCPP_INFO(rclcpp::get_logger("server"), "Goal Canceled");
+      return;
+    }
+    // Update sequence
+    sequence.push_back(sequence[i] + sequence[i - 1]);
+    // Publish feedback
+    goal_handle->publish_feedback(feedback);
+    RCLCPP_INFO(rclcpp::get_logger("server"), "Publish Feedback");
+
+    loop_rate.sleep();
+  }
+
+  // Check if goal is done
+  if (rclcpp::ok()) {
+    result_response->sequence = sequence;
+    goal_handle->set_succeeded(result_response);
+    RCLCPP_INFO(rclcpp::get_logger("server"), "Goal Suceeded");
+  }
+}
+
+void handle_accepted(const std::shared_ptr<GoalHandleFibonacci> goal_handle)
+{
+  // this needs to return quickly to avoid blocking the executor, so spin up a new thread
+  std::thread{execute, goal_handle}.detach();
+}
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("minimal_action_server");
+
+  // Create an action server with three callbacks
+  //   'handle_goal' and 'handle_cancel' are called by the Executor (rclcpp::spin)
+  //   'execute' is called whenever 'handle_goal' returns by accepting a goal
+  //    Calls to 'execute' are made in an available thread from a pool of four.
+  auto action_server = rclcpp_action::create_server<Fibonacci>(
+    node,
+    "fibonacci",
+    handle_goal,
+    handle_cancel,
+    handle_accepted);
+
+  rclcpp::spin(node);
+
+  rclcpp::shutdown();
+  return 0;
+}
