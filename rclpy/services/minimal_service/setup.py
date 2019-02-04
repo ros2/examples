f51ade13a6b2bf4ef722dfed11ac486368c4ee03
Shane Loretz <shane.loretz@gmail.com>
2019-02-04 09:10:24 -0800

    Make examples install entry point scripts into a package (#226)
    * Install examples...client package
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    
    * Install examples...service package
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    
    * Use package_name variable
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    
    * Add __init__.py
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    
    * Install examples...publisher package
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>
    
    * Install examples...subscriber package
    
    Signed-off-by: Shane Loretz <sloretz@osrfoundation.org>

--- rclpy/services/minimal_service/setup.py
+++ rclpy/services/minimal_service/setup.py
@@ -5,10 +5,7 @@ package_name = 'examples_rclpy_minimal_service'
 setup(
     name=package_name,
     version='0.6.1',
-    packages=[],
-    py_modules=[
-        'service',
-        'service_member_function'],
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -32,8 +29,9 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'service = service:main',
-            'service_member_function = service_member_function:main',
+            'service = examples_rclpy_minimal_service.service:main',
+            'service_member_function = '
+            ' examples_rclpy_minimal_service.service_member_function:main',
         ],
     },
 )
