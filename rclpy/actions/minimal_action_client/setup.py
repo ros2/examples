cfc9bc6f2d329e4b7bdf41606f09654140e698e1
Jacob Perron <jacob@openrobotics.org>
2019-03-05 10:11:25 -0800

    rclpy action examples (#222)
    * Copy rclpy action examples from #216
    
    * Bump version of examples_rclpy_action to match other packages
    
    * Consolidate composable action client example
    
    * Add action client example that is not composable
    
    * Wait for action server
    
    * Restructure into separate packages for action client and action server examples
    
    This package structure is consistent with examples for services and topics.
    
    * Update API in action server examples
    
    * Rename action server examples
    
    Now the 'simplest' example is 'server.py'.
    
    * Add action server example that is not composable
    
    * Update setup.py
    
    * Fix syntax
    
    * Update action client examples to use goal handle API for the result
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Improve action client output and result handling
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Update action server examples
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Move action examples into Python packages
    
    This avoid top-level module names from clashing when installed.
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Add action client cancel example
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Address review comments
    
    * Update author
    * Update copyright year
    * Shutdown client example after result received
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * GoalResponse.ACCEPT_AND_EXECUTE -> GoalResponse.ACCEPT
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Fix client_cancel example
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Remove race from server_single_goal example
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Add action server example that defers the execution of an accepted goal
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Reduce the timer period for the client cancel example
    
    This makes it easy to experiment with the scenario where a deferred goal is canceled prior to execution.
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Support canceling goals with non-composable action server example
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Add action server example that queues goals
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>
    
    * Address review
    
    - Fix comment
    - Add author tag to package.xml
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

---
+++ rclpy/actions/minimal_action_client/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+
+package_name = 'examples_rclpy_minimal_action_client'
+
+setup(
+    name=package_name,
+    version='0.6.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    author='Jacob Perron',
+    author_email='jacob@openrobotics.org',
+    maintainer='Shane Loretz',
+    maintainer_email='sloretz@openrobotics.org',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Examples of action clients using rclpy.',
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'client = ' + package_name + '.client:main',
+            'client_cancel = ' + package_name + '.client_cancel:main',
+            'client_not_composable = ' + package_name + '.client_not_composable:main',
+        ],
+    },
+)
