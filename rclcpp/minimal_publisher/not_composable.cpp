15364483994c975ac24ffb14b092922858505fdd
William Woodall <william@osrfoundation.org>
2019-05-05 14:58:58 -0700

    avoid deprecated publish signature
    Signed-off-by: William Woodall <william@osrfoundation.org>

--- rclcpp/minimal_publisher/not_composable.cpp
+++ rclcpp/minimal_publisher/not_composable.cpp
@@ -29,13 +29,13 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("minimal_publisher");
   auto publisher = node->create_publisher<std_msgs::msg::String>("topic");
-  auto message = std::make_shared<std_msgs::msg::String>();
+  auto message = std_msgs::msg::String();
   auto publish_count = 0;
   rclcpp::WallRate loop_rate(500ms);
 
   while (rclcpp::ok()) {
-    message->data = "Hello, world! " + std::to_string(publish_count++);
-    RCLCPP_INFO(node->get_logger(), "Publishing: '%s'", message->data.c_str());
+    message.data = "Hello, world! " + std::to_string(publish_count++);
+    RCLCPP_INFO(node->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher->publish(message);
     rclcpp::spin_some(node);
     loop_rate.sleep();
