eeedec91618e7341bbb4b56b7eb64a81c30134c7
William Woodall <william@osrfoundation.org>
2019-05-06 09:29:33 -0700

    simplify signature
    Signed-off-by: William Woodall <william@osrfoundation.org>

--- rclcpp/minimal_publisher/not_composable.cpp
+++ rclcpp/minimal_publisher/not_composable.cpp
@@ -29,7 +29,7 @@ int main(int argc, char * argv[])
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("minimal_publisher");
   auto publisher = node->create_publisher<std_msgs::msg::String>("topic");
-  auto message = std_msgs::msg::String();
+  std_msgs::msg::String message;
   auto publish_count = 0;
   rclcpp::WallRate loop_rate(500ms);
 
