bafddbdb4957a9e193b849928b67c54d2bdfac86
Karsten Knese <Karsten1987@users.noreply.github.com>
2019-06-14 14:05:11 -0700

    make member function const (#247)
    Signed-off-by: Karsten Knese <karsten@openrobotics.org>

--- rclcpp/minimal_subscriber/member_function.cpp
+++ rclcpp/minimal_subscriber/member_function.cpp
@@ -29,7 +29,7 @@ public:
   }
 
 private:
-  void topic_callback(const std_msgs::msg::String::SharedPtr msg)
+  void topic_callback(const std_msgs::msg::String::SharedPtr msg) const
   {
     RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
   }
