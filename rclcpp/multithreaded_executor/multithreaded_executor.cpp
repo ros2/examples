e483811cc31e8220d9ac15286252646862b8a742
jhdcs <48914066+jhdcs@users.noreply.github.com>
2019-10-08 14:12:15 -0400

    Implemented Multithreaded Executor example (#251)
    * Distro A; OPSEC #2893
    Release Version of Multithreaded Executor example
    
    Signed-off-by: Jacob Hassold <jhassold@dcscorp.com>

---
+++ rclcpp/multithreaded_executor/multithreaded_executor.cpp
@@ -0,0 +1,177 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include <thread>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+/**
+ * A small convenience function for converting a thread ID to a string
+ **/
+std::string string_thread_id()
+{
+  auto hashed = std::hash<std::thread::id>()(std::this_thread::get_id());
+  return std::to_string(hashed);
+}
+
+/* For this example, we will be creating a publishing node like the one in minimal_publisher.
+ * We will have a single subscriber node running 2 threads. Each thread loops at different speeds, and
+ * just repeats what it sees from the publisher to the screen.
+ */
+
+class PublisherNode : public rclcpp::Node
+{
+public:
+  PublisherNode()
+  : Node("PublisherNode")
+  {
+    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+    auto timer_callback =
+      [this]() -> void {
+        auto message = std_msgs::msg::String();
+        message.data = "Hello World! " + std::to_string(this->count_++);
+
+        // Extract current thread
+        auto curr_thread = string_thread_id();
+
+        // Prep display message
+        auto info_message = "\n<<THREAD " + curr_thread + ">> Publishing '%s'";
+        RCLCPP_INFO(this->get_logger(), info_message, message.data.c_str());
+        this->publisher_->publish(message);
+      };
+    timer_ = this->create_wall_timer(500ms, timer_callback);
+  }
+
+private:
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  size_t count_;
+};
+
+class DualThreadedNode : public rclcpp::Node
+{
+public:
+  DualThreadedNode()
+  : Node("DualThreadedNode")
+  {
+    /* These define the callback groups
+     * They don't really do much on their own, but they have to exist in order to
+     * assign callbacks to them. They're also what the executor looks for when trying to run multiple threads
+     */
+    callback_group_subscriber1_ = this->create_callback_group(
+      rclcpp::callback_group::CallbackGroupType::MutuallyExclusive);
+    callback_group_subscriber2_ = this->create_callback_group(
+      rclcpp::callback_group::CallbackGroupType::MutuallyExclusive);
+
+    // Each of these callback groups is basically a thread
+    // Everything assigned to one of them gets bundled into the same thread
+    auto sub1_opt = rclcpp::SubscriptionOptions();
+    sub1_opt.callback_group = callback_group_subscriber1_;
+    auto sub2_opt = rclcpp::SubscriptionOptions();
+    sub2_opt.callback_group = callback_group_subscriber2_;
+
+    subscription1_ = this->create_subscription<std_msgs::msg::String>(
+      "topic",
+      rclcpp::QoS(10),
+      // std::bind is sort of C++'s way of passing a function
+      // If you're used to function-passing, skip these comments
+      std::bind(
+        &DualThreadedNode::subscriber1_cb,  // First parameter is a reference to the function
+        this,                               // What the function should be bound to
+        std::placeholders::_1),             // At this point we're not positive of all the
+                                            // parameters being passed
+                                            // So we just put a generic placeholder
+                                            // into the binder
+                                            // (since we know we need ONE parameter)
+      sub1_opt);                  // This is where we set the callback group.
+                                  // This subscription will run with callback group subscriber1
+
+    subscription2_ = this->create_subscription<std_msgs::msg::String>(
+      "topic",
+      rclcpp::QoS(10),
+      std::bind(
+        &DualThreadedNode::subscriber2_cb,
+        this,
+        std::placeholders::_1),
+      sub2_opt);
+  }
+
+private:
+  /**
+   * Simple function for generating a timestamp
+   * Used for somewhat ineffectually demonstrating that the multithreading doesn't cripple performace
+   */
+  std::string timing_string()
+  {
+    rclcpp::Time time = this->now();
+    return std::to_string(time.nanoseconds());
+  }
+
+  /**
+   * Every time the Publisher publishes something, all subscribers to the topic get poked
+   * This function gets called when Subscriber1 is poked (due to the std::bind we used when defining it)
+   */
+  void subscriber1_cb(const std_msgs::msg::String::SharedPtr msg)
+  {
+    auto message_received_at = timing_string();
+
+    // Extract current thread
+    auto thread_string = "THREAD " + string_thread_id();
+    thread_string += " => Heard '%s' at " + message_received_at;  // Prep display message
+    RCLCPP_INFO(this->get_logger(), thread_string, msg->data.c_str());
+  }
+
+  /**
+   * This function gets called when Subscriber2 is poked
+   * Since it's running on a separate thread than Subscriber 1, it will run at (more-or-less) the same time!
+   */
+  void subscriber2_cb(const std_msgs::msg::String::SharedPtr msg)
+  {
+    auto message_received_at = timing_string();
+
+    // Extract current thread
+    auto thread_string = "THREAD " + string_thread_id();
+
+    // Prep display message
+    thread_string += " => Heard '%s' at " + message_received_at;
+    RCLCPP_INFO(this->get_logger(), thread_string, msg->data.c_str());
+  }
+
+  rclcpp::callback_group::CallbackGroup::SharedPtr callback_group_subscriber1_;
+  rclcpp::callback_group::CallbackGroup::SharedPtr callback_group_subscriber2_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription1_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription2_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+
+  // You MUST use the MultiThreadedExecutor to use, well, multiple threads
+  rclcpp::executors::MultiThreadedExecutor executor;
+  auto pubnode = std::make_shared<PublisherNode>();
+  auto subnode = std::make_shared<DualThreadedNode>();  // This contains BOTH subscriber callbacks.
+                                                        // They will still run on different threads
+                                                        // One Node. Two callbacks. Two Threads
+  executor.add_node(pubnode);
+  executor.add_node(subnode);
+  executor.spin();
+  rclcpp::shutdown();
+  return 0;
+}
