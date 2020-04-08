1ec66492cf6d3fd2ef3048fdd434fb74657ab74c
William Woodall <william@osrfoundation.org>
2020-04-08 05:12:53 -0700

    break examples into separate files
    Signed-off-by: William Woodall <william@osrfoundation.org>

--- rclcpp/wait_set/wait_set.cpp
+++ rclcpp/wait_set/wait_set.cpp
@@ -25,114 +25,80 @@ int main(int argc, char * argv[])
 
   auto do_nothing = [](std_msgs::msg::String::UniquePtr){};
 
-  // Normal WaitSet example
+  auto node = std::make_shared<rclcpp::Node>("wait_set_example_node");
+  auto sub = node->create_subscription<std_msgs::msg::String>("~/chatter", 10, do_nothing);
+  rclcpp::SubscriptionOptions so;
+  so.use_intra_process_comm = rclcpp::IntraProcessSetting::Enable;
+  auto sub2 = node->create_subscription<std_msgs::msg::String>("~/chatter", 10, do_nothing, so);
+  auto guard_condition = std::make_shared<rclcpp::GuardCondition>();
+  auto guard_condition2 = std::make_shared<rclcpp::GuardCondition>();
+
+  rclcpp::WaitSet wait_set(
+    std::vector<rclcpp::WaitSet::SubscriptionEntry>{{sub}},
+    std::vector<rclcpp::GuardCondition::SharedPtr>{guard_condition});
+  wait_set.add_subscription(sub2);
+  wait_set.add_guard_condition(guard_condition2);
+
   {
-    auto node = std::make_shared<rclcpp::Node>("wait_set_example_node");
-    auto sub = node->create_subscription<std_msgs::msg::String>("~/chatter", 10, do_nothing);
-    rclcpp::SubscriptionOptions so;
-    so.use_intra_process_comm = rclcpp::IntraProcessSetting::Enable;
-    auto sub2 = node->create_subscription<std_msgs::msg::String>("~/chatter", 10, do_nothing, so);
-    auto guard_condition = std::make_shared<rclcpp::GuardCondition>();
-    auto guard_condition2 = std::make_shared<rclcpp::GuardCondition>();
-
-    rclcpp::WaitSet wait_set(
-      std::vector<rclcpp::WaitSet::SubscriptionEntry>{{sub}},
-      std::vector<rclcpp::GuardCondition::SharedPtr>{guard_condition});
-    wait_set.add_subscription(sub2);
-    wait_set.add_guard_condition(guard_condition2);
-
-    {
-      auto wait_result = wait_set.wait(std::chrono::seconds(1));
-      assert(wait_result.kind() == rclcpp::WaitResultKind::Timeout);
-    }
-
-    guard_condition->trigger();
-
-    {
-      auto wait_result = wait_set.wait(std::chrono::seconds(1));
-      assert(wait_result.kind() == rclcpp::WaitResultKind::Ready);
-      assert(wait_result.get_wait_set().get_rcl_wait_set().guard_conditions[0] != nullptr);
-      assert(wait_result.get_wait_set().get_rcl_wait_set().guard_conditions[1] == nullptr);
-      assert(wait_result.get_wait_set().get_rcl_wait_set().subscriptions[0] == nullptr);
-      assert(wait_result.get_wait_set().get_rcl_wait_set().subscriptions[1] == nullptr);
-    }
-
-    wait_set.remove_guard_condition(guard_condition2);
-
-    {
-      // still fails with timeout
-      auto wait_result = wait_set.wait(std::chrono::seconds(1));
-      assert(wait_result.kind() == rclcpp::WaitResultKind::Timeout);
-    }
-
-    wait_set.remove_guard_condition(guard_condition);
-
-    {
-      // still fails with timeout
-      auto wait_result = wait_set.wait(std::chrono::seconds(1));
-      assert(wait_result.kind() == rclcpp::WaitResultKind::Timeout);
-    }
-
-    wait_set.remove_subscription(sub2);
-
-    {
-      // still fails with timeout
-      auto wait_result = wait_set.wait(std::chrono::seconds(1));
-      assert(wait_result.kind() == rclcpp::WaitResultKind::Timeout);
-    }
-
-    auto pub = node->create_publisher<std_msgs::msg::String>("~/chatter", 1);
-    pub->publish(std_msgs::msg::String().set__data("test"));
-
-    {
-      auto wait_result = wait_set.wait(std::chrono::seconds(1));
-      assert(wait_result.kind() == rclcpp::WaitResultKind::Ready);
-      assert(wait_result.get_wait_set().get_rcl_wait_set().subscriptions[0] != nullptr);
-      std_msgs::msg::String msg;
-      rclcpp::MessageInfo msg_info;
-      assert(sub->take(msg, msg_info));
-      assert(msg.data == "test");
-    }
-
-    wait_set.remove_subscription(sub);
-
-    {
-      // now fails (fast) with empty
-      auto wait_result = wait_set.wait(std::chrono::seconds(1));
-      assert(wait_result.kind() == rclcpp::WaitResultKind::Empty);
-    }
+    auto wait_result = wait_set.wait(std::chrono::seconds(1));
+    assert(wait_result.kind() == rclcpp::WaitResultKind::Timeout);
   }
 
-  // StaticWaitSet example
+  guard_condition->trigger();
+
   {
-    auto guard_condition = std::make_shared<rclcpp::GuardCondition>();
-    auto guard_condition2 = std::make_shared<rclcpp::GuardCondition>();
-
-    rclcpp::StaticWaitSet<0, 1, 0, 0> static_wait_set(
-      std::array<rclcpp::StaticWaitSet<0, 1, 0, 0>::SubscriptionEntry, 0>{},
-      std::array<rclcpp::GuardCondition::SharedPtr, 1>{{guard_condition}},
-      std::array<rclcpp::TimerBase::SharedPtr, 0>{},
-      std::array<rclcpp::StaticWaitSet<0, 1, 0, 0>::WaitableEntry, 0>{});
-    // Note: The following line will result in a compiler error, since the
-    //   static storage policy prevents editing after construction.
-    // static_wait_set.add_guard_condition(guard_condition2);
-    // static_wait_set.remove_guard_condition(guard_condition2);
-    (void)guard_condition2;
-
-    {
-      auto wait_result = static_wait_set.wait(std::chrono::seconds(1));
-      assert(wait_result.kind() == rclcpp::WaitResultKind::Timeout);
-    }
+    auto wait_result = wait_set.wait(std::chrono::seconds(1));
+    assert(wait_result.kind() == rclcpp::WaitResultKind::Ready);
+    assert(wait_result.get_wait_set().get_rcl_wait_set().guard_conditions[0] != nullptr);
+    assert(wait_result.get_wait_set().get_rcl_wait_set().guard_conditions[1] == nullptr);
+    assert(wait_result.get_wait_set().get_rcl_wait_set().subscriptions[0] == nullptr);
+    assert(wait_result.get_wait_set().get_rcl_wait_set().subscriptions[1] == nullptr);
   }
 
-  // ThreadSafeWaitSet example
-  // {
-  //   auto guard_condition = std::make_shared<rclcpp::GuardCondition>();
-  //   auto guard_condition2 = std::make_shared<rclcpp::GuardCondition>();
+  wait_set.remove_guard_condition(guard_condition2);
 
-  //   rclcpp::WaitSet wait_set({{guard_condition}});
-  //   wait_set.add_guard_condition(guard_condition2);
-  // }
+  {
+    // still fails with timeout
+    auto wait_result = wait_set.wait(std::chrono::seconds(1));
+    assert(wait_result.kind() == rclcpp::WaitResultKind::Timeout);
+  }
+
+  wait_set.remove_guard_condition(guard_condition);
+
+  {
+    // still fails with timeout
+    auto wait_result = wait_set.wait(std::chrono::seconds(1));
+    assert(wait_result.kind() == rclcpp::WaitResultKind::Timeout);
+  }
+
+  wait_set.remove_subscription(sub2);
+
+  {
+    // still fails with timeout
+    auto wait_result = wait_set.wait(std::chrono::seconds(1));
+    assert(wait_result.kind() == rclcpp::WaitResultKind::Timeout);
+  }
+
+  auto pub = node->create_publisher<std_msgs::msg::String>("~/chatter", 1);
+  pub->publish(std_msgs::msg::String().set__data("test"));
+
+  {
+    auto wait_result = wait_set.wait(std::chrono::seconds(1));
+    assert(wait_result.kind() == rclcpp::WaitResultKind::Ready);
+    assert(wait_result.get_wait_set().get_rcl_wait_set().subscriptions[0] != nullptr);
+    std_msgs::msg::String msg;
+    rclcpp::MessageInfo msg_info;
+    assert(sub->take(msg, msg_info));
+    assert(msg.data == "test");
+  }
+
+  wait_set.remove_subscription(sub);
+
+  {
+    // now fails (fast) with empty
+    auto wait_result = wait_set.wait(std::chrono::seconds(1));
+    assert(wait_result.kind() == rclcpp::WaitResultKind::Empty);
+  }
 
   return 0;
 }
