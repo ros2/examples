52df3122308d24b6cf450b5afff09713a5a2fd32
Jacob Perron <jacob@openrobotics.org>
2020-09-15 14:28:16 -0700

    Update goal response callback signature
    The signature changed in https://github.com/ros2/rclcpp/pull/1311
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- rclcpp/actions/minimal_action_client/member_functions.cpp
+++ rclcpp/actions/minimal_action_client/member_functions.cpp
@@ -88,9 +88,8 @@ private:
   rclcpp::TimerBase::SharedPtr timer_;
   bool goal_done_;
 
-  void goal_response_callback(std::shared_future<GoalHandleFibonacci::SharedPtr> future)
+  void goal_response_callback(GoalHandleFibonacci::SharedPtr goal_handle)
   {
-    auto goal_handle = future.get();
     if (!goal_handle) {
       RCLCPP_ERROR(this->get_logger(), "Goal was rejected by server");
     } else {
