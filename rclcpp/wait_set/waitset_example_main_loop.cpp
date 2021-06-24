4c7cf9ac1e9ad45d01580224ade62ff66388d9a1
Carlos San Vicente <carlos.sanvicente@apex.ai>
2021-06-24 16:42:23 +0200

    Refactor publisher in a node class
    Signed-off-by: Carlos San Vicente <carlos.sanvicente@apex.ai>
    Signed-off-by: William Woodall <william@osrfoundation.org>

--- rclcpp/wait_set/waitset_example_main_loop.cpp
+++ rclcpp/wait_set/waitset_example_main_loop.cpp
@@ -2,6 +2,59 @@
 #include <std_msgs/msg/string.hpp>
 #include <random>
 
+class RandomPublisher : public rclcpp::Node
+{
+public:
+  RandomPublisher()
+      : Node("random_publisher"), count_(0)
+  {
+    auto qos = rclcpp::QoS(rclcpp::KeepLast(10)).transient_local();
+    pub1_ = this->create_publisher<std_msgs::msg::String>("topic_A", qos);
+    pub2_ = this->create_publisher<std_msgs::msg::String>("topic_B", qos);
+    pub3_ = this->create_publisher<std_msgs::msg::String>("topic_C", qos);
+
+    auto run_random_publish = [this]() {
+      std_msgs::msg::String msg1, msg2, msg3;
+      std::vector<std::function<void()>> publishers;
+      msg1.data = "A";
+      msg2.data = "B";
+      msg3.data = "C";
+
+      publishers.emplace_back(([&](){pub1_->publish(msg1);}));
+      publishers.emplace_back(([&](){pub2_->publish(msg2);}));
+      publishers.emplace_back(([&](){pub3_->publish(msg3);}));
+      unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+      std::default_random_engine e(seed);
+      std::vector<int> msgs_indexes{0, 1, 2};
+      std::vector<std::string> msgs_data{"A", "B", "C"};
+
+      while (rclcpp::ok()) {
+        std::shuffle (msgs_indexes.begin(), msgs_indexes.end(), e);
+        RCLCPP_INFO(this->get_logger(), "%s%s%s",
+                    msgs_data[msgs_indexes.at(0)].c_str(),
+                    msgs_data[msgs_indexes.at(1)].c_str(),
+                    msgs_data[msgs_indexes.at(2)].c_str());
+        publishers.at(msgs_indexes.at(0))();
+        publishers.at(msgs_indexes.at(1))();
+        publishers.at(msgs_indexes.at(2))();
+
+        ++count_;
+        std::this_thread::sleep_for(std::chrono::milliseconds(2500));
+      }
+    };
+
+    thread_ = std::thread(run_random_publish);
+  }
+
+private:
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub1_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub2_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr pub3_;
+  std::thread thread_;
+  size_t count_;
+};
+
+
 int32_t main(const int32_t argc, char ** const argv)
 {
   rclcpp::init(argc, argv);
@@ -11,133 +64,26 @@ int32_t main(const int32_t argc, char ** const argv)
 
   auto qos = rclcpp::QoS(rclcpp::KeepLast(10)).transient_local();
 
-  auto sub1 = node->create_subscription<std_msgs::msg::String>("a11", qos, do_nothing);
-  auto sub2 = node->create_subscription<std_msgs::msg::String>("a22", qos, do_nothing);
-  auto sub3 = node->create_subscription<std_msgs::msg::String>("a33", qos, do_nothing);
-
-  std_msgs::msg::String msg1;
-  // msg1.data = "Hello";
-  msg1.data = "A";
-
-  std_msgs::msg::String msg2;
-  // msg2.data = "world";
-  msg2.data = "B";
-
-  std_msgs::msg::String msg3;
-  // msg3.data = "from waitset";
-  msg3.data = "C";
+  auto sub1 = node->create_subscription<std_msgs::msg::String>("topic_A", qos, do_nothing);
+  auto sub2 = node->create_subscription<std_msgs::msg::String>("topic_B", qos, do_nothing);
+  auto sub3 = node->create_subscription<std_msgs::msg::String>("topic_C", qos, do_nothing);
 
   rclcpp::WaitSet wait_set;
   wait_set.add_subscription(sub1);
   wait_set.add_subscription(sub2);
   wait_set.add_subscription(sub3);
 
-  rclcpp::Node publisher_node("publisher_node");
-  const auto pub1 =
-    publisher_node.create_publisher<std_msgs::msg::String>("a11", qos);
-  const auto pub2 =
-    publisher_node.create_publisher<std_msgs::msg::String>("a22", qos);
-  const auto pub3 =
-    publisher_node.create_publisher<std_msgs::msg::String>("a33", qos);
+  RandomPublisher publisher_node;
 
-  std::vector<std::function<void()>> random_publisher;
-  random_publisher.emplace_back(([&](){pub1->publish(msg1);}));
-  random_publisher.emplace_back(([&](){pub2->publish(msg2);}));
-  random_publisher.emplace_back(([&](){pub3->publish(msg3);}));
-  auto thread = std::thread([&random_publisher, &publisher_node]() {
-    auto count{0};
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    std::default_random_engine e(seed);
-    std::vector<int> indexes{0, 1, 2};
-    std::vector<std::string> msgs{"A", "B", "C"};
-    while (rclcpp::ok()) {
-      std::shuffle (indexes.begin(), indexes.end(), e);
-      RCLCPP_INFO(publisher_node.get_logger(), "%s%s%s",
-                  msgs[indexes.at(0)].c_str(),
-                  msgs[indexes.at(1)].c_str(),
-                  msgs[indexes.at(2)].c_str());
-      random_publisher.at(indexes.at(0))();
-      random_publisher.at(indexes.at(1))();
-      random_publisher.at(indexes.at(2))();
-      ++count;
-      std::this_thread::sleep_for(std::chrono::milliseconds(2500));
-    }
-  });
-  /*
-  auto thread = std::thread(
-      [msg1, pub1, msg2, pub2, msg3, pub3, &publisher_node]() {
-
-        auto count{0};
-        while (rclcpp::ok()) {
-
-          // optional: make publish order random
-          // i.e:
-          // ABC, CBA, BAC, BCA, ....
-          // if different rates are used:
-          // AB, CBA, BAC, BCA, ....
-          // currently is always: ACB, ACB
-          pub1->publish(msg1);
-          RCLCPP_INFO(publisher_node.get_logger(), "%s", msg1.data.c_str());
-
-          if (count % 2 == 0) {
-            if ((count/2) % 2 == 0) {
-              pub3->publish(msg3);
-              RCLCPP_INFO(publisher_node.get_logger(), "%s", msg3.data.c_str());
-            }
-
-            pub2->publish(msg2);
-            RCLCPP_INFO(publisher_node.get_logger(), "%s", msg2.data.c_str());
-          }
-          ++count;
-          std::this_thread::sleep_for(std::chrono::milliseconds(2500));
-        }
-      });
-      */
-/*
-  auto thread1 = std::thread(
-    [msg1, pub1, &publisher_node]() {
-      while (rclcpp::ok()) {
-
-        pub1->publish(msg1);
-        // std::cout << "Publishing msg1: " << msg1.data << std::endl;
-        RCLCPP_INFO(publisher_node.get_logger(), "%s", msg1.data.c_str());
-        std::this_thread::sleep_for(std::chrono::milliseconds(2500));
-      }
-    });
-
-  auto thread2 = std::thread(
-    [msg2, pub2, &publisher_node]() {
-      while (rclcpp::ok()) {
-        pub2->publish(msg2);
-        // std::cout << "Publishing msg2: " << msg2.data << std::endl;
-        RCLCPP_INFO(publisher_node.get_logger(), "%s", msg2.data.c_str());
-        std::this_thread::sleep_for(std::chrono::milliseconds(5000));
-      }
-    });
-
-  auto thread3 = std::thread(
-    [msg3, pub3, &publisher_node]() {
-      while (rclcpp::ok()) {
-        pub3->publish(msg3);
-        // std::cout << "Publishing msg3: " << msg3.data << std::endl;
-        RCLCPP_INFO(publisher_node.get_logger(), "%s", msg3.data.c_str());
-        std::this_thread::sleep_for(std::chrono::milliseconds(10000));
-      }
-    });
-*/
-
-  // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   auto num_recv = std::size_t();
   while (rclcpp::ok()) {
-    // Waiting up to 5s for a sample to arrive.
     const auto wait_result = wait_set.wait(std::chrono::seconds(5));
 
     if (wait_result.kind() == rclcpp::WaitResultKind::Ready) {
-      // Use sub1 as triggering condition for all subs?
-
       bool expected_sub_triggered =
         wait_result.get_wait_set().get_rcl_wait_set().subscriptions[0U] &&
-        wait_result.get_wait_set().get_rcl_wait_set().subscriptions[1U];
+        wait_result.get_wait_set().get_rcl_wait_set().subscriptions[1U] &&
+        wait_result.get_wait_set().get_rcl_wait_set().subscriptions[2U];
 
       if (expected_sub_triggered) {
         std_msgs::msg::String msg1;
@@ -155,15 +101,11 @@ int32_t main(const int32_t argc, char ** const argv)
         }
         if (sub3->take(msg3, msg_info)) {
           ++num_recv;
-          RCLCPP_INFO(
-              node->get_logger(), "%s%s%s",
-              msg1.data.c_str(), msg2.data.c_str(), msg3.data.c_str());
-        } else {
-          RCLCPP_INFO(
-              node->get_logger(), "%s%s",
-              msg1.data.c_str(), msg2.data.c_str());
         }
-        // std::cout << "I heard: " << msg1.data << msg2.data << msg3.data << std::endl;
+        // handle topics A B C all together
+        RCLCPP_INFO(
+            node->get_logger(), "%s%s%s",
+            msg1.data.c_str(), msg2.data.c_str(), msg3.data.c_str());
       }
     } else if (wait_result.kind() == rclcpp::WaitResultKind::Timeout) {
       std::cerr << "No message received after 5s." << std::endl;
@@ -173,12 +115,5 @@ int32_t main(const int32_t argc, char ** const argv)
   }
 
   rclcpp::shutdown();
-
-  thread.join();
-  /*
-  thread1.join();
-  thread2.join();
-  thread3.join();
-  */
   return 0;
 }
