41be33312324796ddfa6ce1a084c083a89372d41
Aditya Pande <aditya050995@gmail.com>
2021-09-30 11:28:23 -0700

    Fixed package.xml, setup.py
    Signed-off-by: Aditya Pande <aditya050995@gmail.com>

--- launch_testing/launch_testing_examples/setup.py
+++ launch_testing/launch_testing_examples/setup.py
@@ -4,7 +4,7 @@ package_name = 'launch_testing_examples'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='0.12.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
@@ -15,8 +15,8 @@ setup(
     zip_safe=True,
     maintainer='aditya',
     maintainer_email='aditya050995@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
+    description='Examples of simple launch tests',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
