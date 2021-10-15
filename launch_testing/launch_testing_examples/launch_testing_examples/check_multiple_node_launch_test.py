5d7b82cd8b424e2e3f9835f76ddaaf1164536339
Aditya Pande <aditya050995@gmail.com>
2021-10-15 15:34:13 -0700

    Split test in 2 files, added WaitForNodes utility
    Signed-off-by: Aditya Pande <aditya050995@gmail.com>

--- launch_testing/launch_testing_examples/launch_testing_examples/check_multiple_node_launch_test.py
+++ launch_testing/launch_testing_examples/launch_testing_examples/check_multiple_node_launch_test.py
@@ -13,7 +13,8 @@
 # limitations under the License.
 
 
-import sys
+import random
+import string
 import time
 import unittest
 
@@ -26,22 +27,15 @@ import pytest
 import rclpy
 from rclpy.node import Node
 
-import yaml
-
 
 @pytest.mark.launch_test
 @launch_testing.markers.keep_alive
 def generate_test_description():
     # Default number of nodes to be launched
-    n = 5
-    for item in sys.argv[::-1]:
-        if 'number_of_nodes:=' in item:
-            n = int(item.strip('number_of_nodes:='))
-
-    print('Launching ', n, 'nodes !')
-
+    n = 2
     node_list = []
     node_names = []
+
     for i in range(n):
         node_list.append(
             launch_ros.actions.Node(
@@ -52,52 +46,112 @@ def generate_test_description():
         )
         node_names.append('demo_node_' + str(i))
 
-    # Record all the data in a rosbag
-    node_list.append(
-        launch.actions.ExecuteProcess(
-            cmd=['ros2', 'bag', 'record', '-a', '-o', 'test_bag'],
-            output='screen'
-        )
-    )
-
     node_list.append(launch_testing.actions.ReadyToTest())
-
     return launch.LaunchDescription(node_list), {'node_list': node_names}
 
 
 class TestFixture(unittest.TestCase):
 
-    def test_node_start(self, proc_output, node_list):
+    def test_nodes_successful(self, node_list):
         """Check if all the nodes were launched correctly."""
-        rclpy.init()
-        node = DummyNode('test_node')
-        time.sleep(3)
-        result = node.wait_for_nodes(node_list, 8.0)
-        rclpy.shutdown()
-        assert result, 'Nodes not found!'
+        time.sleep(1.0)
+
+        # Method 1
+        wait_for_nodes_1 = WaitForNodes(node_list, timeout=5.0)
+        assert wait_for_nodes_1.wait()
+        assert wait_for_nodes_1.get_nodes_not_found() == set()
+        wait_for_nodes_1.shutdown()
+
+        # Method 2
+        with WaitForNodes(node_list, timeout=5.0) as wait_for_nodes_2:
+            print('All nodes were found !')
+            assert wait_for_nodes_2.get_nodes_not_found() == set()
+
+    def test_nodes_unsuccessful(self, node_list):
+        """Insert a invalid node name that should not exist."""
+        time.sleep(1.0)
+        invalid_node_list = node_list + ['invalid_node']
+
+        # Method 1
+        wait_for_nodes_1 = WaitForNodes(invalid_node_list, timeout=1.0)
+        assert not wait_for_nodes_1.wait()
+        assert wait_for_nodes_1.get_nodes_not_found() == {'invalid_node'}
+        wait_for_nodes_1.shutdown()
+
+        # Method 2
+        with pytest.raises(RuntimeError):
+            with WaitForNodes(invalid_node_list, timeout=1.0):
+                pass
 
 
-@launch_testing.post_shutdown_test()
-class TestFixtureAfterShutdown(unittest.TestCase):
+# TODO (adityapande-1995): Move WaitForNodes implementation to launch_testing_ros
+# after https://github.com/ros2/rclpy/issues/831 is resolved
+class WaitForNodes:
+    """
+    Wait to discover supplied nodes.
 
-    def test_rosbag_record(self):
-        """Check if the rosbag2 recording was successful."""
-        with open('test_bag/metadata.yaml', 'r') as stream:
-            metadata = yaml.safe_load(stream)
-            assert metadata['rosbag2_bagfile_information']['message_count'] > 0
+    Example usage:
+    --------------
+    # Method 1, calling wait() and shutdown() manually
+    def method_1():
+        node_list = ['foo', 'bar']
+        wait_for_nodes = WaitForNodes(node_list, timeout=5.0)
+        assert wait_for_nodes.wait()
+        print('Nodes found!')
+        assert wait_for_nodes.get_nodes_not_found() == set()
+        wait_for_nodes.shutdown()
 
+    # Method 2, using the 'with' keyword
+    def method_2():
+        with WaitForNodes(['foo', 'bar'], timeout=5.0) as wait_for_ndoes:
+            assert wait_for_nodes.get_nodes_not_found() == set()
+            print('Nodes found!')
+    """
 
-class DummyNode(Node):
+    def __init__(self, node_names, timeout=5.0):
+        self.node_names = node_names
+        self.timeout = timeout
+        self.__ros_context = rclpy.Context()
+        rclpy.init(context=self.__ros_context)
+        self._prepare_node()
 
-    def __init__(self, name='test_node'):
-        super().__init__(name)
+        self.__expected_nodes_set = set(node_names)
+        self.__nodes_found = None
 
-    def wait_for_nodes(self, node_names, timeout=8.0):
+    def _prepare_node(self):
+        self.__node_name = '_test_node_' +\
+            ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+        self.__ros_node = Node(node_name=self.__node_name, context=self.__ros_context)
+
+    def wait(self):
         start = time.time()
         flag = False
-        print('Waiting for nodes...')
-        while time.time() - start < timeout and not flag:
-            flag = all(name in self.get_node_names() for name in node_names)
-            time.sleep(0.1)
+        print('Waiting for nodes')
+        while time.time() - start < self.timeout and not flag:
+            flag = all(name in self.__ros_node.get_node_names() for name in self.node_names)
+            time.sleep(0.3)
 
+        self.__nodes_found = set(self.__ros_node.get_node_names())
+        self.__nodes_found.remove(self.__node_name)
         return flag
+
+    def shutdown(self):
+        self.__ros_node.destroy_node()
+        rclpy.shutdown(context=self.__ros_context)
+
+    def __enter__(self):
+        if not self.wait():
+            raise RuntimeError('Did not find all nodes !')
+
+        return self
+
+    def __exit__(self, exep_type, exep_value, trace):
+        if exep_type is not None:
+            raise Exception('Exception occured, value: ', exep_value)
+        self.shutdown()
+
+    def get_nodes_found(self):
+        return self.__nodes_found
+
+    def get_nodes_not_found(self):
+        return self.__expected_nodes_set - self.__nodes_found
---
+++ launch_testing/launch_testing_examples/launch_testing_examples/record_rosbag_launch_test.py
@@ -0,0 +1,73 @@
+# Copyright 2021 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import shutil
+import tempfile
+import time
+import unittest
+
+import launch
+import launch.actions
+import launch_ros.actions
+import launch_testing.actions
+import launch_testing.markers
+import pytest
+
+import yaml
+
+
+@pytest.mark.launch_test
+@launch_testing.markers.keep_alive
+def generate_test_description():
+    rosbag_dir = tempfile.mkdtemp() + '/test_bag'
+
+    node_list = [
+        launch_ros.actions.Node(
+            executable='talker',
+            package='demo_nodes_cpp',
+            name='demo_node'
+        ),
+        launch.actions.ExecuteProcess(
+            cmd=['ros2', 'bag', 'record', '-a', '-o', rosbag_dir],
+            output='screen'
+        ),
+        launch_testing.actions.ReadyToTest()
+    ]
+
+    return launch.LaunchDescription(node_list), {'rosbag_dir': rosbag_dir}
+
+
+class TestFixture(unittest.TestCase):
+
+    def test_delay(self):
+        """Delay the shutdown of processes so that rosbag can record some messages."""
+        time.sleep(3)
+
+
+@launch_testing.post_shutdown_test()
+class TestFixtureAfterShutdown(unittest.TestCase):
+
+    def test_rosbag_record(self, rosbag_dir):
+        """Check if the rosbag2 recording was successful."""
+        with open(rosbag_dir + '/metadata.yaml', 'r') as stream:
+            metadata = yaml.safe_load(stream)
+            assert metadata['rosbag2_bagfile_information']['message_count'] > 0
+            print('The following topics received messages:')
+            for item in metadata['rosbag2_bagfile_information']['topics_with_message_count']:
+                print(item['topic_metadata']['name'], 'recieved ', item['message_count'],
+                      ' messages')
+
+        # Delete the rosbag directory
+        shutil.rmtree(rosbag_dir.replace('/test_bag', ''))
