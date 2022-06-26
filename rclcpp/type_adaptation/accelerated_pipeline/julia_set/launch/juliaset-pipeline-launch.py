5b78f3122d12b3325168a0b2382eea13d14c2944
Swapnesh Wani <swani@nvidia.com>
2022-06-26 21:22:29 +0000

    Adding copyright message.

--- rclcpp/type_adaptation/accelerated_pipeline/julia_set/launch/juliaset-pipeline-launch.py
+++ rclcpp/type_adaptation/accelerated_pipeline/julia_set/launch/juliaset-pipeline-launch.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Open Source Robotics Foundation, Inc.
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- rclcpp/type_adaptation/accelerated_pipeline/simple_increment/launch/inc-pipeline-launch.py
+++ rclcpp/type_adaptation/accelerated_pipeline/simple_increment/launch/inc-pipeline-launch.py
@@ -1,3 +1,4 @@
+# Copyright (c) 2021, NVIDIA CORPORATION.  All rights reserved.
 # Copyright 2021 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
