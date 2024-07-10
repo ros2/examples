b0fa8023bddfc6d0ba2567e8b41317b7fc370ea5
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-07-10 17:26:50 +0200

    Removed outdated comment
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- rclcpp/actions/minimal_action_client/member_functions.cpp
+++ rclcpp/actions/minimal_action_client/member_functions.cpp
@@ -21,7 +21,6 @@
 
 #include "example_interfaces/action/fibonacci.hpp"
 #include "rclcpp/rclcpp.hpp"
-// TODO(jacobperron): Remove this once it is included as part of 'rclcpp.hpp'
 #include "rclcpp_action/rclcpp_action.hpp"
 
 class MinimalActionClient : public rclcpp::Node
