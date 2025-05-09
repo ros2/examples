61ec35922041c77a2d157eb3aa121fb3c907185c
jncfa <20467009+jncfa@users.noreply.github.com>
2025-05-10 00:56:54 +0200

    improve readibility of reported thread ids
    Signed-off-by: jncfa <20467009+jncfa@users.noreply.github.com>

--- rclcpp/executors/multithreaded_executor/multithreaded_executor.cpp
+++ rclcpp/executors/multithreaded_executor/multithreaded_executor.cpp
@@ -16,6 +16,8 @@
 #include <memory>
 #include <string>
 #include <thread>
+#include <algorithm>
+#include <vector>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
@@ -27,8 +29,13 @@ using namespace std::chrono_literals;
  **/
 std::string string_thread_id()
 {
-  auto hashed = std::hash<std::thread::id>()(std::this_thread::get_id());
-  return std::to_string(hashed);
+  static std::vector<std::thread::id> known_thread_ids {};
+  const auto thread_it = std::find(known_thread_ids.cbegin(), known_thread_ids.cend(), std::this_thread::get_id());
+  if (thread_it != known_thread_ids.end()){
+    return std::to_string(std::distance(known_thread_ids.cbegin(), thread_it));
+  }
+  known_thread_ids.push_back(std::this_thread::get_id());
+  return std::to_string(known_thread_ids.size() - 1);
 }
 
 /* For this example, we will be creating a publishing node like the one in minimal_publisher.
