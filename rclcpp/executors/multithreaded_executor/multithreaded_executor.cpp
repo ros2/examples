294ee1994cad48b7291a6d60d84ad47c1486d00c
jncfa <20467009+jncfa@users.noreply.github.com>
2025-05-11 11:17:21 +0200

    apply changes from review
    Signed-off-by: jncfa <20467009+jncfa@users.noreply.github.com>

--- rclcpp/executors/multithreaded_executor/multithreaded_executor.cpp
+++ rclcpp/executors/multithreaded_executor/multithreaded_executor.cpp
@@ -11,12 +11,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <algorithm>
 #include <chrono>
 #include <functional>
 #include <memory>
 #include <string>
 #include <thread>
-#include <algorithm>
 #include <vector>
 
 #include "rclcpp/rclcpp.hpp"
