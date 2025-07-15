b3cb0191d3f63dd8192c59292280925ec2e87bc4
mosfet80 <10235105+mosfet80@users.noreply.github.com>
2025-07-15 09:37:42 +0200

    Fix setuptools deprecations (#421)
    Signed-off-by: Mos <realeandrea@yahoo.it>

--- rclpy/actions/minimal_action_server/setup.py
+++ rclpy/actions/minimal_action_server/setup.py
@@ -20,13 +20,16 @@ setup(
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
     description='Examples of action servers using rclpy.',
     license='Apache License, Version 2.0',
-    tests_require=['pytest'],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
             'server = ' + package_name + '.server:main',
