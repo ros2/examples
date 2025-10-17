680b820cdd93bb05d99666703e6a12402e9689bc
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-10-17 11:23:17 +0200

    0.21.3

--- launch_testing/launch_testing_examples/setup.py
+++ launch_testing/launch_testing_examples/setup.py
@@ -4,7 +4,7 @@ package_name = 'launch_testing_examples'
 
 setup(
     name=package_name,
-    version='0.21.2',
+    version='0.21.3',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
