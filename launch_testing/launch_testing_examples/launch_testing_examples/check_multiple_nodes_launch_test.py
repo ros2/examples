12c8b3dcb85ab9c286c814fcd6589db7ac3aeace
yadunund <yadunund@gmail.com>
2026-07-23 09:24:27 -0700

    Run tests with RMW isolation (#451)
    Signed-off-by: Yadunund Vijay <yadunund@gmail.com>
    (cherry picked from commit 90a5b6428c1d2817d604f6483ce6b9452f66ac71)

--- launch_testing/launch_testing_examples/launch_testing_examples/check_multiple_nodes_launch_test.py
+++ launch_testing/launch_testing_examples/launch_testing_examples/check_multiple_nodes_launch_test.py
@@ -23,6 +23,7 @@ import launch.actions
 import launch_ros.actions
 import launch_testing.actions
 import launch_testing.markers
+from launch_testing_ros.actions import EnableRmwIsolation
 import pytest
 import rclpy
 from rclpy.node import Node
@@ -100,7 +101,7 @@ class WaitForNodes:
 @pytest.mark.launch_test
 @launch_testing.markers.keep_alive
 def generate_test_description():
-    launch_actions = []
+    launch_actions = [EnableRmwIsolation()]
     node_names = []
 
     for i in range(3):
